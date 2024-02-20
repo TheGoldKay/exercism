1f94b376d986f9636d3003e1d32b4db872e73cdc
TheGoldKay <jhonnatanwfc@live.com>
2024-02-20 16:11:21 -0300

    security checks for ManUnited

---
+++ ans/playground/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Xunit;
+
+void Main()
+{
+    var num1 = 100;
+    int num2 = 100;
+    Assert.Equal(num1, num2);
+    Assert.Equal(num1.GetType(), num2.GetType());
+    Console.WriteLine("All Tests Passed!");
+    float n = 1.23f;
+    var ns = (int)n;
+    bool t = true;
+    var ts = (int)t;
+    Console.WriteLine($"{ts}");
+}
+
+
+Main();
